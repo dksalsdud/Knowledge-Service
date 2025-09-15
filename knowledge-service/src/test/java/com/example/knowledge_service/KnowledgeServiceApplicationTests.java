25004dff123291797741a0fd161b077c1489ac72
dksalsdud <dkslasdud@nsu.ac.kr>
2025-09-15 13:58:34 +0000

    스프링 초기 세팅

---
+++ knowledge-service/src/test/java/com/example/knowledge_service/KnowledgeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.knowledge_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnowledgeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
