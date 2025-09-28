935832673ad7c6cbbad257df6973c39c5662bf25
dksalsdud <dkslasdud@nsu.ac.kr>
2025-09-28 09:44:47 +0000

    [feat] HomeController 추가
    Notiva 홈페이지의 로그인 전 회원가입과 로그인을 유도하는 홈 페이지로써 우리가 무엇을 하는 웹사이트인지 간단한 소개 등이 나와있는 홈 페이지다

---
+++ knowledge-service/src/main/java/com/example/knowledge_service/home/HomeController.java
@@ -0,0 +1,17 @@
+package com.example.knowledge_service.home;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class HomeController {
+    
+    //notiva home controller
+    
+    @GetMapping("/home")
+    public String home() {
+        return "home";
+    }
+    
+}
