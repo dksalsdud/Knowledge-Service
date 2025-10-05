a6881624edb249b23ce2a0cdb6730b8903b6038d
dksalsdud <dkslasdud@nsu.ac.kr>
2025-10-05 14:37:24 +0000

    [feat] NoteService 추가
    추후 개발 예정

---
+++ knowledge-service/src/main/java/com/example/knowledge_service/user/service/UserService.java
@@ -0,0 +1,15 @@
+package com.example.knowledge_service.user.service;
+
+import org.springframework.stereotype.Service;
+
+// import com.example.knowledge_service.user.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    
+    // private final UserRepository userRepository;
+    
+}
