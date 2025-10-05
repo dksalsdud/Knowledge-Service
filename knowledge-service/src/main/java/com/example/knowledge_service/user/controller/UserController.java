03d6dcca77354d0d0d4bd8fc4c85963becf078ea
dksalsdud <dkslasdud@nsu.ac.kr>
2025-10-05 14:34:05 +0000

    [feat] UserController 추가
    추후 개발 예정

---
+++ knowledge-service/src/main/java/com/example/knowledge_service/user/controller/UserController.java
@@ -0,0 +1,19 @@
+package com.example.knowledge_service.user.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+// import com.example.knowledge_service.user.service.UserService;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequestMapping("/user")
+@RequiredArgsConstructor
+public class UserController {
+
+    // private final UserService userService;
+
+    
+    
+}
