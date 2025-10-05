e1bd112796f926dad724365b03b32f6e50ca752f
dksalsdud <dkslasdud@nsu.ac.kr>
2025-10-05 14:29:48 +0000

    [feat] NoteDTO 추가
    controller단에서 넘어오는 노트 제목 및 내용을 가져오기 위한 DTO

---
+++ knowledge-service/src/main/java/com/example/knowledge_service/note/dto/NoteDTO.java
@@ -0,0 +1,13 @@
+package com.example.knowledge_service.note.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class NoteDTO {
+
+    private String title;
+
+    private String content;
+}
