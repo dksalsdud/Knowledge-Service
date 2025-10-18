165e5fe8b0d66e5e9927bf47a6f030c8a1001dd5
dksalsdud <dkslasdud@nsu.ac.kr>
2025-10-18 05:20:21 +0000

    [refactor] BookmarkRepository 어노테이션 추가
    북마크 레포지토리 어노테이션 추가

--- knowledge-service/src/main/java/com/example/knowledge_service/bookmark/repository/BookmarkRepository.java
+++ knowledge-service/src/main/java/com/example/knowledge_service/bookmark/repository/BookmarkRepository.java
@@ -6,9 +6,11 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import com.example.knowledge_service.bookmark.domain.Bookmark;
 
+@Repository
 public interface BookmarkRepository extends JpaRepository<Bookmark, Long>{
     
     /**
