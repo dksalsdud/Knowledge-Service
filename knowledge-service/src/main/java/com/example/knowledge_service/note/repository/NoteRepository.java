8d21360a43d100eb8cf0208c53160604b2c1e7b6
dksalsdud <dkslasdud@nsu.ac.kr>
2025-10-18 05:36:29 +0000

    [refactor] NoteRepository 어노테이션 추가
    노트 레포지토리에 레포지토리 어노테이션을 추가

--- knowledge-service/src/main/java/com/example/knowledge_service/note/repository/NoteRepository.java
+++ knowledge-service/src/main/java/com/example/knowledge_service/note/repository/NoteRepository.java
@@ -3,12 +3,14 @@ package com.example.knowledge_service.note.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.knowledge_service.note.domain.Note;
 
 /**
  * 노트 데이터 접근을 담당하는 리포지토리
  */
+@Repository
 public interface NoteRepository extends JpaRepository<Note, Long>{
 
     /**
