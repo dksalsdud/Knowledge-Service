091dac6890d11d8313c60d814762b1a23d29fc18
dksalsdud <dkslasdud@nsu.ac.kr>
2025-10-23 14:27:22 +0000

    초기 개발 세팅

--- notiva/src/main/java/com/example/knowledge_service/config/SecurityConfig.java
+++ notiva/src/main/java/com/example/knowledge_service/config/SecurityConfig.java
@@ -12,6 +12,9 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http
+
+            .csrf(csrf->csrf.disable())
+
             .authorizeHttpRequests(auth -> auth
                 .anyRequest().permitAll()
             );
