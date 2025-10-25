ffc1791978d76ca5b392a790dab87baf2f08b985
dksalsdud <dkslasdud@nsu.ac.kr>
2025-10-25 07:39:15 +0000

    [feat] NoteService 의존성 추가

--- notiva/src/main/java/com/example/knowledge_service/note/controller/NoteController.java
+++ notiva/src/main/java/com/example/knowledge_service/note/controller/NoteController.java
@@ -3,6 +3,8 @@ package com.example.knowledge_service.note.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.knowledge_service.note.service.NoteService;
+
 import lombok.RequiredArgsConstructor;
 
 @Controller
@@ -10,5 +12,6 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/note")
 public class NoteController {
 
+    private final NoteService noteService;
     
 }
