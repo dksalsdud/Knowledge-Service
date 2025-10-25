464783fa1fe4b1326065134cad768794805b6e09
dksalsdud <dkslasdud@nsu.ac.kr>
2025-10-25 07:21:50 +0000

    [comment] 주석 변경

--- notiva/src/main/java/com/example/knowledge_service/home/controller/NotivaHomeController.java
+++ notiva/src/main/java/com/example/knowledge_service/home/controller/NotivaHomeController.java
@@ -6,8 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class NotivaHomeController{
     
-    //notiva home controller
-    
+    /**
+     * 노티바 로그인 전 홈 컨트롤러
+     * @return templates /home/home
+     */
     @GetMapping("/home")
     public String home() {
         
