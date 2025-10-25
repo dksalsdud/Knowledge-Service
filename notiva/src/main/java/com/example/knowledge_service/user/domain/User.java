a889264a444d8af5581dba659eae05ebfe54b350
dksalsdud <dkslasdud@nsu.ac.kr>
2025-10-25 13:24:24 +0000

    [feat] 이메일 컬럼 추가 null 불가, 중복 불가, 길이 100자리 까지 제한

--- notiva/src/main/java/com/example/knowledge_service/user/domain/User.java
+++ notiva/src/main/java/com/example/knowledge_service/user/domain/User.java
@@ -1,5 +1,6 @@
 package com.example.knowledge_service.user.domain;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -21,4 +22,7 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String email;
 }
