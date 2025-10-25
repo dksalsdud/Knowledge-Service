27abd5c4d2527588b38b21f1fcd17f39401583e3
dksalsdud <dkslasdud@nsu.ac.kr>
2025-10-25 13:43:03 +0000

    [feat] UserRepository interface 추가

---
+++ notiva/src/main/java/com/example/knowledge_service/user/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.knowledge_service.user.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.knowledge_service.user.domain.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{
+    
+}
