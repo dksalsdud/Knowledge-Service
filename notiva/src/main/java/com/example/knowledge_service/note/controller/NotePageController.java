6f6f347a1aed0b4e59cbc116b0d8c658169576b5
dksalsdud <dkslasdud@nsu.ac.kr>
2025-10-25 10:57:47 +0000

    [feat] 노트 상세 페이지 추가
    사용자가 노트의  자세한 내용을 보고 싶을 때
    사용자에게 노트 정보를 보여주는 컨트롤러 페이지

--- notiva/src/main/java/com/example/knowledge_service/note/controller/NotePageController.java
+++ notiva/src/main/java/com/example/knowledge_service/note/controller/NotePageController.java
@@ -3,6 +3,7 @@ package com.example.knowledge_service.note.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -31,5 +32,18 @@ public class NotePageController {
 
         return "note/list";
     }
-    
+
+    /**
+     * 노트 상세 페이지 조회
+     * @param id 노트 ID
+     * @param model 뷰에 전달할 데이터 모델
+     * @return 노트 상세 페이지 뷰
+     */
+    @GetMapping("/{id}")
+    public String getNoteDetailPage(@PathVariable("id") Long id, Model model) {
+
+        model.addAttribute("detailNote", noteService.getDetailNote(id));
+
+        return "note/detail";
+    }
 }
