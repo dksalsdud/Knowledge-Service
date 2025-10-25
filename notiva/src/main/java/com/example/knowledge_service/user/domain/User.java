fdfef625b70360ecb333db77da1abab6f6c92599
dksalsdud <dkslasdud@nsu.ac.kr>
2025-10-25 13:40:06 +0000

    [feat] @PrePersist 추가 엔티티 객체 생성되기 전에 실행 되는 메서드로 만들어진 시간을 저장함

--- notiva/src/main/java/com/example/knowledge_service/user/domain/User.java
+++ notiva/src/main/java/com/example/knowledge_service/user/domain/User.java
@@ -7,6 +7,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -42,4 +43,13 @@ public class User {
 
     @Column(nullable = false)
     private LocalDateTime createdAt;
+
+    /**
+     * 엔티티 저장 전 실행
+     * 생성 시간을 현재 시간으로 초기화
+     */
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
 }
