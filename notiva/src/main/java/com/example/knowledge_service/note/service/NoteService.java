09a0f484ce7aad5c2141a06b0ef80099de2a9c68
dksalsdud <dkslasdud@nsu.ac.kr>
2025-10-25 07:39:46 +0000

    [feat] NoteRepository 의존성 추가

--- notiva/src/main/java/com/example/knowledge_service/note/service/NoteService.java
+++ notiva/src/main/java/com/example/knowledge_service/note/service/NoteService.java
@@ -2,7 +2,13 @@ package com.example.knowledge_service.note.service;
 
 import org.springframework.stereotype.Service;
 
+import com.example.knowledge_service.note.repository.NoteRepository;
+
+import lombok.RequiredArgsConstructor;
+
 @Service
+@RequiredArgsConstructor
 public class NoteService {
     
+    private final NoteRepository noteRepository;
 }
