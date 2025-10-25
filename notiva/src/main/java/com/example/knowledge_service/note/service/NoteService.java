0f98bc31067cf9f56f710d4a4f220a0a492ae981
dksalsdud <dkslasdud@nsu.ac.kr>
2025-10-25 10:13:21 +0000

    [feat] getListNotes method 추가
    사용자 아이디로 모든 노트를 최신순으로 가져오는 함수

--- notiva/src/main/java/com/example/knowledge_service/note/service/NoteService.java
+++ notiva/src/main/java/com/example/knowledge_service/note/service/NoteService.java
@@ -1,7 +1,10 @@
 package com.example.knowledge_service.note.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import com.example.knowledge_service.note.domain.Note;
 import com.example.knowledge_service.note.repository.NoteRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -11,4 +14,14 @@ import lombok.RequiredArgsConstructor;
 public class NoteService {
     
     private final NoteRepository noteRepository;
+
+    /**
+     * 특정 사용자의 모든 노트 목록 조회
+     * @param userId 사용자 ID
+     * @return 노트 목록
+     */
+    public List<Note> getListNotes(Long userId) {
+
+        return noteRepository.findAllByUser_IdOrderByUpdatedAtDesc(userId);
+    }
 }
