2027a60041fac1780235b54f7319196865689717
dksalsdud <dkslasdud@nsu.ac.kr>
2025-10-25 07:36:26 +0000

    [feat] NoteController Class 추가

---
+++ notiva/src/main/java/com/example/knowledge_service/note/controller/NoteController.java
@@ -0,0 +1,14 @@
+package com.example.knowledge_service.note.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/note")
+public class NoteController {
+
+    
+}
