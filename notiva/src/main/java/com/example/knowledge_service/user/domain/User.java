de1f6cc594523692acdef213bd5d32c67107df51
dksalsdud <dkslasdud@nsu.ac.kr>
2025-10-25 13:38:08 +0000

    [feat] 생성 시간 컬럼 추가

--- notiva/src/main/java/com/example/knowledge_service/user/domain/User.java
+++ notiva/src/main/java/com/example/knowledge_service/user/domain/User.java
@@ -1,5 +1,7 @@
 package com.example.knowledge_service.user.domain;
 
+import java.time.LocalDateTime;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
