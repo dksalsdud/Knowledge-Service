42c47aec8e5e46d978ff2b3fb83f468433f56cf3
dksalsdud <dkslasdud@nsu.ac.kr>
2025-10-26 10:18:02 +0000

    [feat] createTodoList method 추가
    유저 아이디로 UserRepository의 findByid 함수를 통해 유저 객체를 찾아옴
    
    없다면 예외처리
    
    있다면 TodoList builer 함수를 통해 DTO 객체를 옮겨 save 함수 호출 후 저장

--- notiva/src/main/java/com/example/knowledge_service/todolist/service/TodoListService.java
+++ notiva/src/main/java/com/example/knowledge_service/todolist/service/TodoListService.java
@@ -4,8 +4,13 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import com.example.knowledge_service.exception.AppException;
+import com.example.knowledge_service.exception.ErrorCode;
 import com.example.knowledge_service.todolist.domain.TodoList;
+import com.example.knowledge_service.todolist.dto.TodoListDTO;
 import com.example.knowledge_service.todolist.repository.TodoListRepository;
+import com.example.knowledge_service.user.domain.User;
+import com.example.knowledge_service.user.repository.UserRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -14,6 +19,7 @@ import lombok.RequiredArgsConstructor;
 public class TodoListService {
 
     private final TodoListRepository todoListRepository;
+    private final UserRepository userRepository;
 
     /**
      * 특정 사용자의 모든 노트 목록 조회
@@ -23,5 +29,29 @@ public class TodoListService {
     public List<TodoList> getTodoLists(Long userId) {
 
         return todoListRepository.findAllByUser_Id(userId);
+    }
+
+    /**
+     * 투드리스트 생성 하는 함수
+     * @param userId
+     * @param todoListDTO
+     * @throws USER_NOT_FOUND
+     */
+    public void createTodoList(Long userId, TodoListDTO todoListDTO) {
+
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> {
+                    return new AppException(ErrorCode.USER_NOT_FOUND, "해당 사용자가 존재 하지 않습니다. userId = " + userId);
+                });
+
+        TodoList todo = TodoList.builder()
+                .title(todoListDTO.getTitile())
+                .complete(todoListDTO.getComplete())
+                .startDay(todoListDTO.getStartDay())
+                .endDay(todoListDTO.getEndDay())
+                .user(user)
+                .build();
+
+        todoListRepository.save(todo);
     }
 }
