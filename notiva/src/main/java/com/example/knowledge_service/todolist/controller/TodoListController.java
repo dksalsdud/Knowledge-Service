982f0bca907ff06907a7c68496d65d40bf641d37
dksalsdud <dkslasdud@nsu.ac.kr>
2025-10-26 11:22:51 +0000

    [feat] 투두 업데이트 페이지 컨트롤러 추가
    업데이트 페이지를 보여주기 위해 해당 투두ID 객체를 가져와 프론트로 보냄

--- notiva/src/main/java/com/example/knowledge_service/todolist/controller/TodoListController.java
+++ notiva/src/main/java/com/example/knowledge_service/todolist/controller/TodoListController.java
@@ -12,6 +12,7 @@ import lombok.RequiredArgsConstructor;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 
@@ -46,6 +47,20 @@ public class TodoListController {
         todoListService.createTodoList(userId, todoListDTO);
         
         return "redirect:/Notiva";
+    }
+    
+    /**
+     * 투두 업데이트 페이지를 보여주는 컨트롤러
+     * @param todoId
+     * @param model
+     * @return todo/update 업데이트 페이지
+     */
+    @GetMapping("/update")
+    public String getTodoListUpdatePage(@PathVariable("todoId") Long todoId, Model model) {
+
+        model.addAttribute("detailTodo", todoListService.detailTodoList(todoId));
+
+        return "todo/update";
     }
     
 }
