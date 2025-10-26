8ea326d257bed6cc77021eaaeb2af84b33f790ed
dksalsdud <dkslasdud@nsu.ac.kr>
2025-10-26 05:14:07 +0000

    [feat] 노티바 홈 컨트롤러 추가 로그인 이후 보여질 홈 화면으로 노트 리스트와 캘린더에 노트 생성 및 수정 시간과 두투리스트 기간 표기 플러스 캘린더에 일정 저장 가능
    현제 노트 리스트만 프론트 단으로 보내게 되어있음 캘린더 및 투두리스트  나중에 추가 예정

--- notiva/src/main/java/com/example/knowledge_service/home/controller/NotivaHomeController.java
+++ notiva/src/main/java/com/example/knowledge_service/home/controller/NotivaHomeController.java
@@ -1,10 +1,20 @@
 package com.example.knowledge_service.home.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.knowledge_service.note.service.NoteService;
+
+import lombok.RequiredArgsConstructor;
+
 
 @Controller
+@RequiredArgsConstructor
 public class NotivaHomeController{
+
+    private final NoteService noteService;
     
     /**
      * 노티바 로그인 전 홈 컨트롤러
@@ -14,5 +24,20 @@ public class NotivaHomeController{
     public String home() {
         
         return "home/home";
+    }
+    
+    /**
+     * 
+     * @param id 유저아이디 사용자 id 필요
+     * @param model 뷰에 전달할 데이터 노트리스트와 노트리스트들의 생성 변경시간, 투두리스트 기간
+     * @return templates /home/notiva
+     */
+    @GetMapping("/Notiva")
+    public String getNoteCalendarTodoHomePage(@RequestParam(value = "userId") Long userId, Model model) {
+
+        // 노트 리스트
+        model.addAttribute("noteList", noteService.getListNotes(userId));
+
+        return "/home/notiva";
     }
 }
