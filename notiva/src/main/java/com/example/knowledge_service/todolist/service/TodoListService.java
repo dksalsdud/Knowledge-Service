98f11c49eac090e3a837114af6c5deddd7f378e7
dksalsdud <dkslasdud@nsu.ac.kr>
2025-10-26 06:42:30 +0000

    [feat] TodoListRepository 의존성 추가

--- notiva/src/main/java/com/example/knowledge_service/todolist/service/TodoListService.java
+++ notiva/src/main/java/com/example/knowledge_service/todolist/service/TodoListService.java
@@ -2,11 +2,13 @@ package com.example.knowledge_service.todolist.service;
 
 import org.springframework.stereotype.Service;
 
+import com.example.knowledge_service.todolist.repository.TodoListRepository;
+
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class TodoListService {
 
-    
+    private final TodoListRepository todoListRepository;
 }
