61b0d773b2bbbdb41137c095c54c549370f4e135
dksalsdud <dkslasdud@nsu.ac.kr>
2025-10-26 07:21:39 +0000

    [feat] TodoListController 추가

---
+++ notiva/src/main/java/com/example/knowledge_service/todolist/controller/TodoListController.java
@@ -0,0 +1,12 @@
+package com.example.knowledge_service.todolist.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/todo")
+public class TodoListController {
+    
+}
