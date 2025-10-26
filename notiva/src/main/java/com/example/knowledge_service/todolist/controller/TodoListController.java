8dc7f98b62ea722d7525acd014ab98d022a0769b
dksalsdud <dkslasdud@nsu.ac.kr>
2025-10-26 10:29:16 +0000

    [feat] 투두리스트 생성 처리 컨트롤러 추가
    투두리스트 처리 컨트롤러 추가 보내기 누를 시 작성된 투두리스트가 데이터베이스에 저장 후
    리다이렉트 노티바 홈 페이지로 이동

--- notiva/src/main/java/com/example/knowledge_service/todolist/controller/TodoListController.java
+++ notiva/src/main/java/com/example/knowledge_service/todolist/controller/TodoListController.java
@@ -2,11 +2,33 @@ package com.example.knowledge_service.todolist.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.knowledge_service.todolist.dto.TodoListDTO;
+import com.example.knowledge_service.todolist.service.TodoListService;
+
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/todo")
 public class TodoListController {
+
+    private final TodoListService todoListService;
+
+    /**
+     * 투두리스트 생성 처리
+     */
+    @PostMapping("/create")
+    public String postTodoList(@RequestParam(value = "userId") Long userId, @ModelAttribute TodoListDTO todoListDTO) {
+        
+        todoListService.createTodoList(userId, todoListDTO);
+        
+        return "redirect:/Notiva";
+    }
     
 }
