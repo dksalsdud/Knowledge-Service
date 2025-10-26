5ed80da52c1d76eb63416bfd8ced01f700562f15
dksalsdud <dkslasdud@nsu.ac.kr>
2025-10-26 07:17:37 +0000

    [feat] getTodoLists method 추가

--- notiva/src/main/java/com/example/knowledge_service/todolist/service/TodoListService.java
+++ notiva/src/main/java/com/example/knowledge_service/todolist/service/TodoListService.java
@@ -1,7 +1,10 @@
 package com.example.knowledge_service.todolist.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import com.example.knowledge_service.todolist.domain.TodoList;
 import com.example.knowledge_service.todolist.repository.TodoListRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -11,4 +14,14 @@ import lombok.RequiredArgsConstructor;
 public class TodoListService {
 
     private final TodoListRepository todoListRepository;
+
+    /**
+     * 특정 사용자의 모든 노트 목록 조회
+     * @param userId 사용자 ID
+     * @return 투두리스트 목록
+     */
+    public List<TodoList> getTodoLists(Long userId) {
+
+        return todoListRepository.findAllByUser_Id(userId);
+    }
 }
