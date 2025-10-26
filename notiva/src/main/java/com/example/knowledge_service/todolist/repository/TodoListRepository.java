848e6f2f4fc6cfc090a42f4058bc58eff794155e
dksalsdud <dkslasdud@nsu.ac.kr>
2025-10-26 07:01:01 +0000

    [feat] findAllByUser_Id 추가

--- notiva/src/main/java/com/example/knowledge_service/todolist/repository/TodoListRepository.java
+++ notiva/src/main/java/com/example/knowledge_service/todolist/repository/TodoListRepository.java
@@ -1,5 +1,7 @@
 package com.example.knowledge_service.todolist.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,5 @@ import com.example.knowledge_service.todolist.domain.TodoList;
 @Repository
 public interface TodoListRepository extends JpaRepository<TodoList, Long> {
     
+    List<TodoList> findAllByUser_Id(Long userId);
 }
