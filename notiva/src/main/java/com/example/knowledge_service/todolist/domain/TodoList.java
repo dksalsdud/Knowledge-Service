088eb88e7b165b5d3715d19b44afff72b0940f7f
dksalsdud <dkslasdud@nsu.ac.kr>
2025-10-26 05:34:55 +0000

    [feat] 제목 컬럼 추가
    null 불가, 길이 100자

--- notiva/src/main/java/com/example/knowledge_service/todolist/domain/TodoList.java
+++ notiva/src/main/java/com/example/knowledge_service/todolist/domain/TodoList.java
@@ -1,5 +1,6 @@
 package com.example.knowledge_service.todolist.domain;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -21,4 +22,7 @@ public class TodoList {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String title;
 }
