e34bfea38499ba013ffb6b704194d9bf0cf68e6d
dksalsdud <dkslasdud@nsu.ac.kr>
2025-10-29 14:13:43 +0000

    [feat] 태그 Cascadetype 설정 및 고아객체 설정
    노트가 생성되기전 태그랑 같이 데이터베이스에 저장 가능
    노트 삭제시 해당 태그들 삭제 가능
    
    고아객체 설정으로 노트와 연결이 끊어진 태그들은 데이터베이스에서 자동 삭제

--- notiva/src/main/java/com/example/knowledge_service/tag/domain/Tag.java
+++ notiva/src/main/java/com/example/knowledge_service/tag/domain/Tag.java
@@ -1,10 +1,18 @@
 package com.example.knowledge_service.tag.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.example.knowledge_service.notetag.domain.NoteTag;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -31,4 +39,16 @@ public class Tag {
      */
     @Column(nullable = false, length = 50)
     private String tagName;
+
+    /**
+     * 태그 Cascadetype 추가
+     * 노트가 생성되기 전 만들때 같이만들 수 있고 노트 삭제시 해당 태그들 삭제
+     * 
+     * 고아 객체 삭제 설정 추가
+     * 노트와 연결이 끊어진 태그들은 DB에서 자동 삭제
+     */
+    @Builder.Default
+    @OneToMany(mappedBy = "tag", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<NoteTag> noteTags = new ArrayList<>();
+
 }
