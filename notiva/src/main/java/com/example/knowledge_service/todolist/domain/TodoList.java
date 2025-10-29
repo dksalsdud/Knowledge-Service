318df5ecd9c9cf855b60f48f891b9277d895968a
dksalsdud <dkslasdud@nsu.ac.kr>
2025-10-29 11:11:42 +0000

    [feat] @Enumerated 어노테이션 추가

--- notiva/src/main/java/com/example/knowledge_service/todolist/domain/TodoList.java
+++ notiva/src/main/java/com/example/knowledge_service/todolist/domain/TodoList.java
@@ -6,6 +6,8 @@ import com.example.knowledge_service.user.domain.User;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -36,6 +38,7 @@ public class TodoList {
 
     // 완수 여부
     @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
     @Builder.Default
     private TodoComplete complete = TodoComplete.FAIL;
 
