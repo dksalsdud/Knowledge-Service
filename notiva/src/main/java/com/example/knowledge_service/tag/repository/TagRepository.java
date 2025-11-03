5556be56c563557b96514be03f9b251e484138bb
dksalsdud <dkslasdud@nsu.ac.kr>
2025-11-03 13:40:11 +0000

    [feat] findByTagName
    태그 이름으로 태그 찾는 함수 반환 Optional 태그엔티티 객체

--- notiva/src/main/java/com/example/knowledge_service/tag/repository/TagRepository.java
+++ notiva/src/main/java/com/example/knowledge_service/tag/repository/TagRepository.java
@@ -1,5 +1,7 @@
 package com.example.knowledge_service.tag.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,8 @@ import com.example.knowledge_service.tag.domain.Tag;
 @Repository
 public interface TagRepository extends JpaRepository<Tag, Long>{
     
+    /**
+     * 태그 이름으로 태그 찾기
+     */
+    Optional<Tag> findByTagName(String tagName);
 }
