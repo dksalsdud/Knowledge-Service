8bfc2b221dd625e808b1a8df04a53503002ed331
dksalsdud <dkslasdud@nsu.ac.kr>
2025-11-09 14:07:16 +0000

    [comment] 주석 추가

---
+++ notiva/src/main/java/com/example/knowledge_service/tag/service/TagFilterService.java
@@ -0,0 +1,16 @@
+package com.example.knowledge_service.tag.service;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+/**
+ * TagFilerService
+ * 정의 : 프론트단에서 보내는 태그들을 데이터베이스에 넣기 전 정렬화 작업
+ * TagService에 의존성 주입되어 함수로 호출 및 사용
+ */
+@Service
+@RequiredArgsConstructor
+public class TagFilterService {
+    
+}
