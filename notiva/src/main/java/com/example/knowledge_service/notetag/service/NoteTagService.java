beb3525232612d7678e0063400d830d172745f82
dksalsdud <dkslasdud@nsu.ac.kr>
2025-11-09 14:07:33 +0000

    [comment] 주석 추가

---
+++ notiva/src/main/java/com/example/knowledge_service/notetag/service/NoteTagService.java
@@ -0,0 +1,14 @@
+package com.example.knowledge_service.notetag.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * NoteTagService
+ * 정의 : 노트와 태그를 관리하는 서비스 로직
+ * 태그로 특정 노트 조회 및 노트 조회 시 모든 태그 조회 등
+ * 컨트롤러나 서비스단에서 의존성 주입 후 사용할 수 있도록 개발하는 것이 목표
+ */
+@Service
+public class NoteTagService {
+    
+}
